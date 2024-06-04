a79bf4489b8d8de6cf644aacb433c4d930c291a1
Michael Dos Santos <michael.dozzantoz@gmail.com>
2024-06-04 14:58:59 +0100

    t

--- src/utils/stripe.ts
+++ src/utils/stripe.ts
@@ -1,8 +1,5 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(
-  "sk_test_51OQRELKa6s8gzuS5oLP3cyowXs5ZfkWupLggLFbqezufYwJTDVMNaNOo473WYex30phcYFa2xSlhwdLkb6vaBHN300SBWtYPEk",
-  {
-    apiVersion: "2024-04-10",
-  }
-);
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2024-04-10",
+});
